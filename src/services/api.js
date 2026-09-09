364325c2e030a535cdb2db6a5015754a802ac954
lee777777 <leen99belal@gmail.com>
2026-09-09 14:17:42 +0400

    changed api2

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API = axios.create({
 baseURL: import.meta.env.DEV 
     ? "http://localhost:3000/api" 
-    : "http://car-care-api.loca.lt/api",
+    : "https://car-care-api.loca.lt/api",
     headers: {
     'bypass-tunnel-reminder': 'true',
   },
